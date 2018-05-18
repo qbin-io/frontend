e611730de96ba8dc294227de20b3cf0365b3a8ca
Moritz Marquardt <git@mo-mar.de>
2018-05-18 20:40:31 +0200

    Add syntax highlighting back to encrypted documents

--- script/decrypt.js
+++ script/decrypt.js
@@ -1,16 +1,24 @@
+var e = document.getElementById("content");
+while (e.firstElementChild != null) e = e.firstElementChild;
+
+var classes = e.className;
+e.className = null;
+
+
 if (sessionStorage.getItem("key")) {
     window.location.hash = sessionStorage.getItem("key");
     sessionStorage.removeItem("key");
 }
 var key = window.location.hash.substr(1);
 document.getElementById("encryption-hash").textContent = "#" + key;
-console.log(base91.decode(document.getElementById("content").textContent));
 triplesec.decrypt({
     key: triplesec.Buffer(key),
     data: triplesec.Buffer(base91.decode(document.getElementById("content").textContent))
 }, function(err, res) {
-    var e = document.getElementById("content");
-    while (e.firstElementChild != null) e = e.firstElementChild;
     e.textContent = res.toString();
+    e.className = classes;
+    setTimeout(function() {
+        Prism.highlightAll();
+    });
     document.getElementById("encrypting").style.display = "none";
 });
